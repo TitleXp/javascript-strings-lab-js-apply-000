525ca388bd54e59699911e191d0fae07424c11e1
darling-token-6272 <titleyugio@gmail.com>
2018-11-14 19:21:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "This is" + specialGuest + "!";
+var greetSpecialGuest = "Hello," + specialGuest + "!";
 
 var topic = "Let's talk about space!";
 
